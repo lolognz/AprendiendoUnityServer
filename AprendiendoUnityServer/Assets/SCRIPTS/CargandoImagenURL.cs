4afb3b11f7dd97146707c74d6e7732a1879c6c0a
Lolo <lolognz@gmail.com>
2018-05-21 12:56:33 -0400

    Escena de carga de imagen desde url con barra y porcentaje de progreso

---
+++ AprendiendoUnityServer/Assets/SCRIPTS/CargandoImagenURL.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CargandoImagenURL : MonoBehaviour {
+
+    public string url;
+    public RawImage imagenWeb;
+    private WWW www;
+    public Image barraCarga;
+    public Text textoCarga;
+
+
+	// Use this for initialization
+	IEnumerator Start () {
+        www = new WWW(url);
+        yield return www;
+        imagenWeb.texture = www.texture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        barraCarga.fillAmount = www.progress;
+        textoCarga.text = www.progress.ToString("00%");
+        //Debug.Log(www.progress);
+	}
+}
